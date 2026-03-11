32aaccb6ce3c154106ad67f91304846ecc657e53
Morten Zwarenstein <Morten.Zwarenstein@kadaster.nl>
2026-03-11 09:36:00 +0100

    feat: styles update

--- recipe-client/src/app/features/home/home.ts
+++ recipe-client/src/app/features/home/home.ts
@@ -1,13 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { AuthService } from '../../core/auth/auth.service';
+import { BowlService } from '../../core/bowl/bowl.service';
 
 @Component({
   selector: 'app-home',
+  imports: [RouterLink],
   templateUrl: './home.html',
 })
 export class HomeComponent {
   private readonly authService = inject(AuthService);
+  private readonly bowlService = inject(BowlService);
 
   protected readonly username = this.authService.getUsername();
+  protected readonly bowl = toSignal(this.bowlService.getBowl());
 }
